0a6911cb4571b7d6043ea6548e52c097642e3c86
Dkounta <dkounta14@gmail.com>
2026-06-03 18:27:35 +0000

    Dkounta : Collecte de l'ordinateur vers le dossier source à l'interieur de data.csv

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 import shutil
-
-
 def collecte(chemin_fichier_externe):
     try:
-        shutil.copy(chemin_fichier_externe, os.path.join("source", "data.csv"))
-        print("[COLLECTE] Succès : Fichier copié avec succès.")
+        nom_fichier = os.path.basename(chemin_fichier_externe)
+        fichier_destination = os.path.join("source", nom_fichier)
+        shutil.copy(chemin_fichier_externe, fichier_destination)
+        print(
+            f"[COLLECTE] Succès : Le fichier '{nom_fichier}' a été extrait et copié dans 'source/'."
+        )
     except Exception as e:
-        print(f"[COLLECTE] Erreur : {e}")
-
-
+        print(f"[COLLECTE] Erreur lors de l'extraction : {e}")
 if __name__ == "__main__":
-    chemin_test = r"C:\Master1\ETL_Pharmacie\data\donnees_csv\Dim_Produit.csv"
+    print("--- Test de la Collecte Dynamique ---")
+    chemin_test = r"C:\Master1\ETL_Pharmacie\data\donnees_csv\Dim_Client.csv"
     collecte(chemin_test)
