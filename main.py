1130c1270c33ed671ac84b50845a011f6e385716
misskadidia149 <kd1807319@gmail.com>
2026-06-03 15:32:54 +0000

    creation de la fonction tl_hive() pour copier ou charger le fichier du dossier hdfs/ vers le dossier hive/ pour la tâche 3

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#ecris juste ta fonction ici
+# Tâche 3: Créer la fonction tl_hive() pour copier ou charger le fichier du dossier hdfs/ vers le dossier hive/
+import shutil, os
+def tl_hive():
+    source_dir = 'hdfs/'
+    destination_dir = 'hive/'
+
+    for file in os.listdir(source_dir):
+        source= os.path.join(source_dir, file)
+        destination=os.path.join(destination_dir,file)
+        shutil.move(source, destination)
+# Appeler la fonction tl_hive() pour exécuter la tâche
+tl_hive()
